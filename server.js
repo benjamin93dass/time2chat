0402e3a59be891d6de1597e08918d05126b28c63
Benjamin Dass <bdas16001@gmail.com>
2019-11-20 13:39:31 -0700

    minor bug 4

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function getPerson(req, response) {
     console.log("Retreiving person with id: ", id);
 
     getPersonFromDb(id, function(err, result){
-        console.log("Back from the getPersonFromDb result: ", res);
+        console.log("Back from the getPersonFromDb result: ", result);
 
         if (error || result == null || result.length != 1) {
             response.status(500).json({success:false, data: error});
