f1e1f873d4e0eaf8cbb2dc9a2afd41002106e645
Benjamin Dass <bdas16001@gmail.com>
2019-12-14 19:16:16 -0700

    bugs 3

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
+const { Pool } = require("pg");
+
+const connectionString = process.env.DATABASE_URL || "postgres://uhzpoksdfjeast:13c2b6104d0fd3d0778bc95d96838684cf006f95b462886b3829821c8fb695d1@ec2-174-129-253-113.compute-1.amazonaws.com:5432/d796pog6c2c07u";
+const pool = new Pool({connectionString: connectionString});
 
 users = [];
 connections = [];
@@ -11,6 +15,7 @@ server.listen(process.env.PORT || 5000);
 app.get('/', function(req, res){
     res.render("main.ejs");
 });
+app.get("/getPerson", getPerson)
 
 io.sockets.on('connection', function(socket){
     connections.push(socket);
@@ -41,3 +46,40 @@ io.sockets.on('connection', function(socket){
         io.sockets.emit('get users', users);
     }
 });
+
+function getPerson(req, response) {
+    console.log("Getting person information.")
+
+    var id = req.query.id;
+    console.log("Retreiving person with id: ", id);
+
+    getPersonFromDb(id, function(error, result){
+        console.log("Back from the getPersonFromDb result: ", result);
+
+        if (error || result == null || result.length != 1) {
+            response.status(500).json({success:false, data: error});
+        } else {
+            response.json(result[0]);
+        }
+
+    });
+};
+
+function getPersonFromDb(id, callback) {
+    console.log("getPersonFromDb called with id: ", id);
+
+    var sql = "SELECT id, first, last, birthdate FROM persondb WHERE id = $1::int";
+    var params = [id];
+
+    pool.query(sql, params, function(err, res) {
+        if (err) {
+            console.log("An error with the DB occured");
+            console.log(err);
+            callback(err, null);
+        }
+
+        console.log("Found db result: " + JSON.stringify(res.rows));
+
+        callback(null, res.rows);
+    })
+}
