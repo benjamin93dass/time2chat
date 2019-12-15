bcd9074ea849bb83dbbd50e6989e35659ce6b44c
Benjamin Dass <bdas16001@gmail.com>
2019-12-14 18:34:31 -0700

    build 1

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
 
 const { Pool } = require("pg");
+users = [];
+connections = [];
 
 const connectionString = process.env.DATABASE_URL || "postgres://uhzpoksdfjeast:13c2b6104d0fd3d0778bc95d96838684cf006f95b462886b3829821c8fb695d1@ec2-174-129-253-113.compute-1.amazonaws.com:5432/d796pog6c2c07u";
 const pool = new Pool({connectionString: connectionString});
 
 app.set("port", (process.env.PORT || 5000));
 
+app.get('/', function(req, res){
+    res.render("main.ejs");
+});
 app.get("/getPerson", getPerson)
 
 app.listen(app.get("port"), function() {
     console.log("Now listening on port for connections on port: ", app.get("port"));
 });
 
+// Database querrying
 function getPerson(req, response) {
     console.log("Getting person information.")
 
@@ -50,3 +58,34 @@ function getPersonFromDb(id, callback) {
         callback(null, res.rows);
     })
 }
+
+// Chat functionality
+io.sockets.on('connection', function(socket){
+    connections.push(socket);
+    console.log("Connection: %s sockets connected", connections.length);
+
+    // Disconnect
+    socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnected: %s sockets disconnected", connections.length);
+    });
+
+    // send message
+    socket.on('send message', function(data){
+        io.sockets.emit("new message", {msg: data, user: socket.username});
+    })
+
+    // new user
+    socket.on("new user", function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames() {
+        io.sockets.emit('get users', users);
+    }
+});
